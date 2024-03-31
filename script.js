cca64c63f9f87a0509f0eccb933dbc91a491f281
HeraldAge <hchimphepojr@gmail.com>
2024-03-31 13:39:02 +0200

    create mxm grid accomplished

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const container=document.querySelector(".container");
-container.style.backgroundColor="red";
+const container = document.querySelector(".container");
+container.style.backgroundColor = "red";
 
-const p=document.createElement("p")
-p.textContent="HELLO!"
+const p = document.createElement("p")
+p.textContent = "HELLO!"
+let gridSize
 
-for(let i=0; i<=20; i++){
-    const div=document.createElement("div")
-    div.style.backgroundColor="green"
-    div.classList="div"
-    container.appendChild(div)
+for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement("div")
+    row.classList = "row"
+    container.appendChild(row)
+    console.log(i + "\n")
+    for (let j = 0; j < gridSize; j++) {
+        const col = document.createElement("div")
+        col.classList = "col"
+        row.appendChild(col)
+        console.log(j)
+    }
 }
