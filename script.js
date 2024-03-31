5dbbcb07fc6ddd2bc53b6a1bfc8594a9a044f19d
HeraldAge <hchimphepojr@gmail.com>
2024-03-31 14:20:36 +0200

    cells change color on hover

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ container.style.backgroundColor = "red";
 
 const p = document.createElement("p")
 p.textContent = "HELLO!"
-let gridSize
-
+let gridSize=4;
+const row = document.createElement("div")
+const col = document.createElement("div")
 for (let i = 0; i < gridSize; i++) {
     const row = document.createElement("div")
-    row.classList = "row"
     container.appendChild(row)
-    console.log(i + "\n")
+    row.classList = "row"
+    
     for (let j = 0; j < gridSize; j++) {
         const col = document.createElement("div")
         col.classList = "col"
         row.appendChild(col)
-        console.log(j)
+        col.addEventListener("mouseover", ()=>{
+            col.style.backgroundColor="cyan"
+        })
     }
 }
+
