6dc111487d3a6f444cbaaf37642125dc64e27f8c
HeraldAge <hchimphepojr@gmail.com>
2024-03-31 18:49:20 +0200

    completed core functionality. UI/UX terrible

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
-const container = document.querySelector(".container");
-container.style.backgroundColor = "red";
+const container = document.createElement("div")
+container.classList="container"
+const body=document.querySelector("body")
+body.appendChild(container)
 
-const p = document.createElement("p")
-p.textContent = "HELLO!"
-let gridSize=4;
-const row = document.createElement("div")
-const col = document.createElement("div")
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement("div")
-    container.appendChild(row)
-    row.classList = "row"
-    
-    for (let j = 0; j < gridSize; j++) {
-        const col = document.createElement("div")
-        col.classList = "col"
-        row.appendChild(col)
-        col.addEventListener("mouseover", ()=>{
-            col.style.backgroundColor="cyan"
-        })
+const btnResetGrid=document.querySelector("#btnGridReset")
+btnResetGrid.classList="btnGridReset"
+
+
+function createGrid(gridSize=8){
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement("div")
+        container.appendChild(row)
+        row.classList = "row"
+        
+        for (let j = 0; j < gridSize; j++) {
+            const col = document.createElement("div")
+            col.classList = "col"
+            row.appendChild(col)
+            col.addEventListener("mouseover", ()=>{
+                col.style.backgroundColor="cyan"
+            })
+        }
     }
 }
+
+function resetGrid(){
+    container.innerHTML='';
+}
+
+btnResetGrid.addEventListener("click", ()=>{
+    let size=prompt("Enter squares per side for the new grid");
+    resetGrid();
+    createGrid(size);
+})
+
+
+
+
+
+
 
